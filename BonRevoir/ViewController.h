c6243d2be769b7634e12a398f41503f3788a4d17
huyl <huyl@users.noreply.github.com>
2014-05-08 02:49:49 -0500

    Exercice

--- BonRevoir/ViewController.h
+++ BonRevoir/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *monLabel;
+@property (weak, nonatomic) IBOutlet UIButton *monBouton;
+- (IBAction)monBouton:(id)sender;
 
 @end
--- BonRevoir/ViewController.m
+++ BonRevoir/ViewController.m
@@ -14,6 +14,9 @@
 
 @implementation ViewController
 
+NSString *const BonjourString = @"Bonjour";
+NSString *const AuRevoirString = @"Au Revoir";
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -26,4 +29,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)monBouton:(id)sender {
+    if ([[_monLabel text] isEqualToString:BonjourString]) {
+        [_monLabel setText:AuRevoirString];
+        [_monBouton setTitle:[@"Dis " stringByAppendingString:BonjourString] forState:UIControlStateNormal];
+    } else {
+        [_monLabel setText:BonjourString];
+        [_monBouton setTitle:[@"Dis " stringByAppendingString:AuRevoirString] forState:UIControlStateNormal];
+    }
+}
 @end
